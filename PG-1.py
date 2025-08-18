e9fba345b62dd861d5c6a79a6c916693647c6211
Bernardo López <bernardolopezf.dev@gmail.com>
2025-08-18 18:13:41 -0500

    Update

---
+++ PG-1.py
@@ -0,0 +1,10 @@
+import string
+letters = string.ascii_letters
+digitss = string.digits
+punctuations = string.punctuation
+
+#print(letters, digitss, punctuations)
+S-P-G = []
+def super_strong_PG ():
+    for i in letters, digitss, punctuations:
+        pass
